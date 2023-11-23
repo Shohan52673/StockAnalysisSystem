ecc01adbe755b3628887529b77a553cc6fe604d8
Shohan Colin <colin@ShohandeMacBook-Air.local>
2023-11-23 11:44:33 +0800

    first commit

---
+++ test03.java
@@ -0,0 +1,20 @@
+public class test03 {
+	public static void main(String[] args) {
+	
+	java.util.Scanner sc =new java.util.Scanner(System.in);
+
+	System.out.print("請輸入成績")
+	int a = sc.nextInt();
+	
+
+
+
+	
+
+
+
+		
+	}
+}
+
+
