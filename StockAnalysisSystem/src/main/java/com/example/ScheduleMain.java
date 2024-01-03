995290a5db78369277aabc0e7d931966d3193708
colin <colin@localhost>
2024-01-03 10:35:43 +0800

    a

--- StockAnalysisSystem/src/main/java/com/example/ScheduleMain.java
+++ StockAnalysisSystem/src/main/java/com/example/ScheduleMain.java
@@ -1,19 +1,12 @@
 package com.example;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Random;
-
-import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.bean.SemiProductStock;
@@ -34,7 +27,7 @@ public class ScheduleMain {
 		
 		semiProductStockDao.save(semiProductStocks);
 		
-		// 通知股票服務，目的為：讓股票服務知道有更新股票資訊，請他廣播給所有 WebSocket的用戶。
+//		 通知股票服務，目的為：讓股票服務知道有更新股票資訊，請他廣播給所有 WebSocket的用戶。
 //		RestTemplate restTemplate = new RestTemplate();
 //		String url = "http://localhost:8080/WebSocketDemo/mvc/stock";
 //		ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
